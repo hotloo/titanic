9720c33e11a4a3242437f47b5d05b8189d891335
Hotloo Xiranood <hotloo.xiranood@gmail.com>
2013-03-28 23:43:10 +0200

    Updated the source code to be able to output the prediction results.

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -1,15 +1,24 @@
 from utils import *
 from sklearn import cross_validation
 from sklearn import svm
+import ipdb
 
 if __name__ == '__main__':
     util = TitaticUtils()
-    (train_label, train_data, test_label, test_data) = util.create_feature_vector()
-    import ipdb;ipdb.set_trace()
-    clf = svm.SVC(kernel = 'rbf', C = 1)
-    kf = cross_validation.KFold(train_label.shape[0],n_folds = 5)
+    (train_label, train_data, test_data) = util.create_feature_vector()
+    kf = cross_validation.KFold(train_label.shape[0],n_folds = 10)
+    best_clf = None
+    best_validation = 0.0
     for train_index,validate_index in kf:
+        clf = svm.SVC(kernel = 'rbf', C = 1)
         clf.fit(train_data[train_index],train_label[train_index])
         vali_score = clf.score(train_data[validate_index],train_label[validate_index])
         train_score = clf.score(train_data[train_index],train_label[train_index])
         print "The result for SVM train[%f] & validation[%f]"%(train_score, vali_score)
+        if vali_score > best_validation:
+            best_clf = clf
+            best_validation = vali_score
+    print "----"
+    print "Selected the SVC with the best validation score[%f]"%best_validation
+    predict_test_label = best_clf.predict(test_data)
+    util.write_file(predict_test_label, '../data/test_result.csv')
--- titanic/utils.py
+++ titanic/utils.py
@@ -2,6 +2,7 @@ import csv as csv
 import numpy as np
 from sklearn import preprocessing
 from collections import Counter
+import ipdb
 
 TRAIN_DATA = '../data/train.csv'
 TEST_DATA = '../data/test.csv'
@@ -10,76 +11,89 @@ class TitaticUtils():
 
     def read_file(self, filename = ""):
         data = []
-        data_size = []
         if not filename:
-            filename = [TRAIN_DATA]
+            filename = [TRAIN_DATA,TEST_DATA]
         for filename_ in filename:
             with open(filename_,'r') as f_handle:
                 csv_file = csv.reader(f_handle)
                 for line in csv_file:
                     data.append(line)
-                data_size.append(len(data))
-        data = np.array(data)
-        return (data,data_size)
+                if filename_ == TRAIN_DATA:
+                    train_data = np.array(data)
+                else:
+                    test_data = np.array(data)
+                data = []
+        return (train_data, test_data)
 
-    def write_file(self, filename = ""):
-        pass
+    def write_file(self, result, filename = ""):
+        output_file = csv.writer(open(filename, 'wb'))
+        test_file = csv.reader(open(TEST_DATA, 'rb'))
+        test_file.next()
+        i = 0
+        for row in test_file:
+            row.insert(0, result[i].astype(np.uint8))
+            output_file.writerow(row)
+            i += 1
 
     def create_feature_vector(self):
-        (data,data_size) = self.read_file()
+        (train_data,test_data) = self.read_file()
         # survival 
-        label = np.array(map(self.get_int, data[1:,0]))
+        label = np.array(map(self.get_int, train_data[1:,0]))
         # pclass
-        _data = self.categorize_column_info(data[1:,1])
+        (pclass_train_data,pclass_test_data) = self.categorize_column_info(train_data[1:,1], test_data[1:,0])
         # name We skip it for now
         # sex
-        _data = np.concatenate((_data,
-                self.categorize_column_info(data[1:,3])), axis = 1)
+        (sex_train_data, sex_test_data) = self.categorize_column_info(train_data[1:,3],test_data[1:,2])
         # age
-        _data = np.concatenate((_data,
-                self.binize_column_info(data[1:,4],10)), axis = 1)
+        (age_train_data, age_test_data) = self.binize_column_info(train_data[1:,4],test_data[1:,3],10)
         # sibsp
-        _data = np.concatenate((_data,
-                self.binize_column_info(data[1:,5],4)), axis = 1)
+        (sibsp_train_data, sibsp_test_data) = self.binize_column_info(train_data[1:,5],test_data[1:,4], 4)
         # parch
-        _data = np.concatenate((_data,
-                self.binize_column_info(data[1:,6],4)), axis = 1)
+        (parch_train_data, parch_test_data) = self.binize_column_info(train_data[1:,6], test_data[1:,5],4)
         # ticket We skip it for now
         # fare
-        _data = np.concatenate((_data,
-                self.binize_column_info(data[1:,8],10)), axis = 1)
+        (fare_train_data, fare_test_data) = self.binize_column_info(train_data[1:,8], test_data[1:,7], 10)
         # cabin We skip it for now
         # embarked
-        _data = np.concatenate((_data,
-                self.categorize_column_info(data[1:,10])), axis = 1)
-        return (label[:data_size[0]], _data[:data_size[0],:],label[data_size[0]:], _data[data_size[0]:,:])
+        (embarked_train_data, embarked_test_data) = self.categorize_column_info(train_data[1:,10], test_data[1:,9])
 
-    def categorize_column_info(self, column):
-        filled_column = self.estimate_missing_values(column)
+        train_data = np.concatenate((pclass_train_data, sex_train_data, age_train_data, sibsp_train_data, parch_train_data, fare_train_data, embarked_train_data), axis = 1)
+        test_data = np.concatenate((pclass_test_data, sex_test_data, age_test_data, sibsp_test_data, parch_test_data, fare_test_data, embarked_test_data), axis = 1)
+        return (label, train_data, test_data)
+
+    def categorize_column_info(self, column, column_test):
+        (filled_column, filled_column_test) = self.estimate_missing_values(column, column_test)
         le = preprocessing.LabelEncoder()
-        le.fit(column)
-        transformed_column = le.transform(column)
+        le.fit(filled_column)
+        transformed_column = le.transform(filled_column)
+        transformed_column_test = le.transform(filled_column_test)
         output_array = np.zeros((filled_column.shape[0], le.classes_.shape[0]))
+        output_array_test = np.zeros((filled_column_test.shape[0], le.classes_.shape[0]))
         for index, item in enumerate(transformed_column.tolist()):
             output_array[index, item] = 1
-        return output_array
+        for index, item in enumerate(transformed_column_test.tolist()):
+            output_array_test[index, item] = 1
+        return (output_array, output_array_test)
 
-    def binize_column_info(self, column, n_bin = 5):
-        filled_column = self.estimate_missing_values(column, estimation_type = 'continuous')
+    def binize_column_info(self, column, column_test, n_bin = 5):
+        (filled_column, filled_column_test) = self.estimate_missing_values(column, column_test, estimation_type = 'continuous')
         max_column = np.max(filled_column)
         int_column = np.apply_along_axis(lambda x,m: np.divide(x,m), 0, filled_column, max_column)
+        int_column_test = np.apply_along_axis(lambda x,m: np.divide(x,m), 0, filled_column_test, max_column)
         bins = np.linspace(np.min(filled_column),np.max(filled_column),n_bin)
         bin_column = np.digitize(int_column, bins, right = True)
+        bin_column_test = np.digitize(int_column_test, bins, right = True)
         output_array = np.zeros((filled_column.shape[0],n_bin))
+        output_array_test = np.zeros((filled_column_test.shape[0],n_bin))
         for index, item in enumerate(bin_column.tolist()):
             output_array[index, item - 1] = 1
-        return output_array
+        for index, item in enumerate(bin_column_test.tolist()):
+            output_array_test[index, item - 1] = 1
+        return (output_array, output_array_test)
 
-    def estimate_missing_values(self, column, estimation_type = 'category'):
+    def estimate_missing_values(self, column, column_test, estimation_type = 'category'):
         if estimation_type == 'category':
             distinct_value_count = Counter(column)
-            if not '' in distinct_value_count.keys():
-                return column
             largest_value = 0
             for key in distinct_value_count:
                 if key == '':
@@ -88,15 +102,20 @@ class TitaticUtils():
                     largest_key = key
                     largest_value = distinct_value_count[key]
             indices = self.get_index(column)
+            indices_test = self.get_index(column_test)
             column[indices] = largest_key
+            column_test[indices_test] = largest_key
         elif estimation_type == 'continuous':
             int_column = map(self.get_float, column)
+            int_column_test = map(self.get_float, column_test)
             new_int_column = np.ma.masked_array(int_column, np.isnan(int_column))
+            new_int_column_test = np.ma.masked_array(int_column_test, np.isnan(int_column_test))
             mean = np.mean(new_int_column)
             column = np.array(map(lambda x: x if not np.isnan(x) else mean, int_column))
+            column_test = np.array(map(lambda x: x if not np.isnan(x) else mean, int_column_test))
         else:
             raise
-        return column
+        return (column, column_test)
 
     def get_index(self, column, value = ''):
         indices = [item for item in xrange(column.shape[0]) if column[item] == value]
@@ -105,6 +124,7 @@ class TitaticUtils():
         return float(s) if s else np.NaN
     def get_int(self,s):
         return int(s) if s else np.NaN
+
 if __name__ == '__main__':
     train = TitaticUtils()
-    (train_label, train_data, test_label, test_data) = train.create_feature_vector()
+    (label, train_data, test_data)= train.create_feature_vector()
