a554cca4d93bf9150b01d2f2ffcbbe4cbf66e4a2
Hotloo Xiranood <hotloo.xiranood@gmail.com>
2013-08-04 19:00:38 +0300

    Some Comments

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -7,14 +7,28 @@ from model import Classifier
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
+
+    # Load the utilities to clean up the data
     util = TitaticUtils()
+
+    # load the feature vector
     (train_label, train_data, test_data) = util.create_feature_vector()
+
+    # shuffle the data
     train_data, train_label = shuffle(train_data, train_label)
+
+    # Load the classifier
     classifier = Classifier()
+
+    # project the feature vector
     (train_pca, test_pca) = classifier.get_pca_projection(train_data, test_data, snum = 2)
+
+    # Prediction
     predict_test_label = Classifier().classify('randomforest', train_label, train_pca, test_pca)
-    #predict_test_label = Classifier().classify('logistic', train_label, train_data, test_data)
+
     util.write_file(predict_test_label, '../data/test_result.csv')
-    plt.scatter(train_pca[:,0],train_pca[:,1], c = train_label)
-    plt.prism
-    plt.show()
+
+    # Visualization
+    #plt.scatter(train_pca[:,0],train_pca[:,1], c = train_label)
+    #plt.prism
+    #plt.show()
